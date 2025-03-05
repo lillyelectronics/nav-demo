ce88681c2017f9bd68a7c0dae1974dc102bc4141
Lilly <lillyelectronics@gmail.com>
2025-03-05 17:20:06 -0600

    -Uses bonus.js as the Help Modal instead of modal.js;

--- app/_layout.js
+++ app/_layout.js
@@ -6,7 +6,7 @@ export default function RootLayout() {
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="product/[id]" options={{ title: "Product Details" }} />
       <Stack.Screen name="category/[name]" options={{ title: "Category" }} />
-      <Stack.Screen name="modal" options={{ presentation: "modal", title: "Help" }} />
+      <Stack.Screen name="bonus" options={{ presentation: "modal", title: "Help" }} />
     </Stack>
   );
 }
